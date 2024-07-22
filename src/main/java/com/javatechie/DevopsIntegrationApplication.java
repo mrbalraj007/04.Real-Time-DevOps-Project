4e41132633ce1244d3f0846146f221030d39ba4d
Balraj Singh <53593772+mrbalraj007@users.noreply.github.com>
2024-07-22 16:57:15 +1000

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to our DevOps Project !!";
+		return "Welcome to our New DevOps Project !!";
 	}
 
 	public static void main(String[] args) {
